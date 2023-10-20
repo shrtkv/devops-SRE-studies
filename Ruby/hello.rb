ea8224b5fee356c593dc16725b9861ecb7ecd2ad
shrtkv <tokoshiro5@gmail.com>
2023-10-20 04:28:49 +0000

    Ruby start (for chief/puppet)

---
+++ Ruby/hello.rb
@@ -0,0 +1,38 @@
+# https://learn.chef.io/courses/course-v1:chef+Ruby101+Perpetual/course/
+#
+=begin 
+	M
+	u
+	l
+	t
+	i
+	lineeee
+=end
+x = 5.0
+y = 3
+
+# addition
+sum = x + y
+puts sum
+
+# subtraction
+sub = x - y
+puts sub
+
+# multiplication
+mul = x * y
+puts mul
+
+# division
+div = x / y
+puts div
+
+# mod
+mod = x % y
+puts mod
+
+nome1 = "Jaqueston"
+hello = "Hello"
+world = " World"
+nome = " nome #{nome1}" 
+puts hello+world+nome
