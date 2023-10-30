a118267da3738e7c91a0843c0918aaa20e4d96e7
shrtkv <tokoshiro5@gmail.com>
2023-10-30 05:27:39 +0000

    main.tf changes

--- archbit.io/terraform/main.tf
+++ archbit.io/terraform/main.tf
@@ -6,28 +6,95 @@ terraform {
     }
   }
 }
-
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region    = var.aws_region
+  profile   = var.aws_profile
 }
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.s3_bucket_name
-}
-
 resource "aws_instance" "my_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
-  availability_zone = var.availability_zone
-  security_groups = [var.security_group_id != "" ? var.security_group_id : var.default_security_group_id]
+  key_name      = var.key_name
+  user_data     = <<-EOF
+    #!/bin/bash
+    apt-get update && apt-get install -y awscli
+    
+  EOF
+
+  vpc_security_group_ids = [aws_security_group.ssh_security_group.id]
+  iam_instance_profile = aws_iam_instance_profile.ec2_s3_instance_profile.name
+  
+}
+resource "aws_s3_bucket" "devopsjrchallenge_bucket" {
+  bucket = var.s3_bucket_name
+}
+resource "aws_iam_role" "ec2_s3_role" {
+  name = "ec2_s3_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+resource "aws_iam_instance_profile" "ec2_s3_instance_profile" {
+  name = "ec2_s3_instance_profile" 
+  role = aws_iam_role.ec2_s3_role.name
+}
+resource "aws_iam_policy" "s3_policy" {
+  name        = "s3_access_policy"
+  description = "Allow EC2 to access S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Effect   = "Allow",
+        Resource = "arn:aws:s3:::${var.s3_bucket_name}/*" 
+      }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "s3_policy_attachment" {
+  policy_arn = aws_iam_policy.s3_policy.arn
+  role       = aws_iam_role.ec2_s3_role.name
 }
 
-resource "aws_subnet" "my_subnet" {
-  count             = var.subnet_id != "" ? 1 : 0
-  cidr_block        = var.subnet_cidr_block
-  vpc_id            = var.vpc_id
-  availability_zone = var.availability_zone
+
+resource "aws_security_group" "ssh_security_group" {
+name        = "ssh_security_group"
+description = "SSH inbound / All outbound"
+vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"           
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  tags = {
+    Name = "devops-jr-challenge"
+  }
 }
+
--- archbit.io/terraform/variables.tf
+++ archbit.io/terraform/variables.tf
@@ -3,7 +3,11 @@ variable "aws_region" {
   type        = string
   default     = "sa-east-1"
 }
-
+variable "aws_profile" {
+  description = "Name of the AWS CLI profile to use"
+  type        = string
+  default     = ""  # Replace with your desired default profile name
+}
 variable "aws_access_key" {
   description = "AWS access key"
   type        = string
@@ -19,13 +23,19 @@ variable "aws_secret_key" {
 variable "ami_id" {
   description = "AMI ID for the EC2 instance"
   type        = string
-  default     = "ami-0af6e9042ea5a4e3e"
+  default     = "ami-034bb51efe941fa1d"
 }
+
 
 variable "instance_type" {
   description = "Type of the EC2 instance"
   type        = string
   default     = "t2.micro"
+}
+variable "aws_session_token" {
+  description = "AWS session token when assuming a role"
+  type        = string
+  default     = ""
 }
 
 variable "s3_bucket_name" {
@@ -34,37 +44,20 @@ variable "s3_bucket_name" {
   default     = "bucket-example-tokoshiro5"
 }
 
-variable "vpc_id" {
-  description = "VPC ID for the EC2 instance"
-  type        = string
-  default     = ""
-}
 
 variable "subnet_id" {
   description = "Subnet ID for the EC2 instance"
   type        = string
   default     = ""
 }
-
-variable "subnet_cidr_block" {
-  description = "CIDR block for the subnet"
+variable "vpc_id" {
+  description = "The ID of the Default VPC where the security group will be created."
   type        = string
-  default     = "10.0.1.0/24"
+  default     = "vpc-0738c08b606c5d37a"
 }
 
-variable "availability_zone" {
-  description = "Availability Zone for the EC2 instance"
+variable "key_name" {
+  description = "The name of the SSH key pair to associate with the EC2 instance."
   type        = string
-  default     = "sa-east-1a"
-}
-
-variable "security_group_id" {
-  description = "Security Group ID for the EC2 instance"
-  type        = string
-  default     = ""
-}
-variable "default_security_group_id" {
-  description = "Default Security Group ID for the EC2 instance"
-  type        = string
-  default     = ""
+  default     = "devopsjrchallenge_rsa"
 }
